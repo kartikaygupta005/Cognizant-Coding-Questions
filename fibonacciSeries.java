cbc9c39207dc8be04c6bacc784cb68e09799fdef
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-09-02 23:47:31 +0530

    without using recursion

---
+++ fibonacciSeries.java
@@ -0,0 +1,18 @@
+import java.util.*;
+// without using recursion
+public class main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0 ; 
+        int b = 1 ; 
+        int c;
+        System.out.print(a + " " + b + " ");
+        for(int i = 2; i < n; i++){
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
