b0ee7ce1f09b7ddf3985f51a628dee8ab923a6db
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-09-03 10:36:44 +0530

    Create SwappingWithoutTemp.java

---
+++ SwappingWithoutTemp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class main{
+    public static vois main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b= sc.nextInt();;
+        n = n+b;
+        b = n-b ;
+        n = n - b;
+        System.out.println("After swapping: n = " + n + ", b = " + b); 
+
+
+    }
+}
